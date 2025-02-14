8af6946b2f4cdc5a70e9a19beea11afe513706d0
Umut <umut.gu@outlook.com>
2025-02-14 21:23:07 +0300

    maven central repo settings

--- common/src/main/java/com/turkcell/common/utility/StringUtil.java
+++
@@ -1,7 +0,0 @@
-package com.turkcell.common.utility;
-
-public class StringUtil {
-    public static void test(){
-        System.out.println("string util test");
-    }
-}
--- customer-service/src/main/java/com/turkcell/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/com/turkcell/customerservice/controller/CustomerController.java
@@ -1,9 +1,9 @@
 package com.turkcell.customerservice.controller;
 
+import com.turkcell.common.utility.StringUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.turkcell.common.utility.StringUtil;
 
 @RestController
 @RequestMapping("/api/v1/customers")
